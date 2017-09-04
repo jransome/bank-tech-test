77a1729ba85fe19a7b99ef3207e7ea716dbfef44
James Ransome <jamesransome@msn.com>
2017-09-04 17:59:33 +0100

    refactor of account record transaction functionality

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,28 @@
+require_relative 'transaction'
+
 class Account
   attr_reader :balance, :transaction_class, :transaction_history
 
-  def initialize(transaction_class)
+  def initialize(transaction_class = Transaction)
     @balance = 0
     @transaction_class = transaction_class
     @transaction_history = []
   end
 
-  def deposit amount
+  def deposit(amount)
     @balance += amount
-    transaction = @transaction_class.new(amount)
-    @transaction_history.push(transaction)
+    record_transaction(amount, @balance)
   end
 
-  def withdraw amount
+  def withdraw(amount)
     @balance -= amount
-    transaction = @transaction_class.new(-amount)
+    record_transaction(-amount, @balance)
+  end
+
+  private
+
+  def record_transaction(amount, new_balance)
+    transaction = @transaction_class.new(amount, @balance)
     @transaction_history.push(transaction)
   end
 end
