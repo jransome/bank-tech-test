699fb632de2e6b323c1ce9628f6f7c43844cb748
James Ransome <jamesransome@msn.com>
2017-09-04 16:51:32 +0100

    passing test for transaction date

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,9 @@
 class Transaction
-  attr_reader :amount, :current_balance
+  attr_reader :amount, :current_balance, :date
 
   def initialize(amount, current_balance)
     @amount = amount
     @current_balance = current_balance
+    @date = Time.now
   end
 end
--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -3,7 +3,14 @@ require 'transaction'
 describe Transaction do
   let(:amount) { 50 }
   let(:current_balance) { 100 }
+  let!(:transaction_date) { Time.now.to_s } #NOTE: .to_s is added to avoid test failing due to nanosecond differences in actual vs expected time values
   subject(:transaction){ described_class.new(amount, current_balance) }
+
+  describe '#date' do
+    it 'records the date of the transaction' do
+      expect(transaction.date.to_s).to eq transaction_date #NOTE: .to_s is added to avoid test failing due to nanosecond differences in actual vs expected time values
+    end
+  end
 
   describe '#amount' do
     it 'records an amount' do
