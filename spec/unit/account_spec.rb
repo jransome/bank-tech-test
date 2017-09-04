15379c7bc2c49a6aec3699908974e44a0bc0993a
James Ransome <jamesransome@msn.com>
2017-09-04 14:48:27 +0100

    test passing for depositing to account

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -2,9 +2,16 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
+  let(:amount) { 400 }
 
   it 'starts with a balance of 0' do
     expect(account.balance).to eq 0
+  end
+
+  describe '#deposit' do
+    it 'increases the account balance when a positive number is supplied' do
+      expect{ account.deposit(amount) }.to change{ account.balance }.by amount
+    end
   end
 
 end
